0b741c524321a23c499cb2f60d79c2aef53003ee
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2023-03-15 04:16:07 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
         <div className="App">
            <ClippedDrawer/>
-            <QuizFooter/>
+            {/*<QuizFooter/>*/}
            {/* <QuizMenu/>*/}
         </div>
     );
--- src/pages/QuizDrower.jsx
+++ src/pages/QuizDrower.jsx
@@ -8,17 +8,18 @@ import QuizHeader from "./QuizHeader";
 import QuizList from "./QuizList";
 import QuizMenu from "./QuizMenu";
 import {useState} from "react";
+import QuizFooter from "./QuizFooter";
 
 const drawerWidth = 240;
 
 export default function ClippedDrawer() {
 
-    const [menuActive, setMenuActive  ] = useState(false)
-    const items = [{value:"Main", href:"/main" }, {value:"Directory", href:"/directory" },
-                    {value:"Rating", href:"/rating" },{value:"Tests", href:"/tests" }];
+    const [menuActive, setMenuActive] = useState(false)
+    const items = [{value: "Main", href: "/main"}, {value: "Directory", href: "/directory"},
+        {value: "Rating", href: "/rating"}, {value: "Tests", href: "/tests"}];
     return (
 
-        <Box sx={{display: 'flex'}}>
+        <Box sx={{display: ''}}>
             <CssBaseline/>
             <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
                 {/*<QuizHeader/>*/}
@@ -29,17 +30,24 @@ export default function ClippedDrawer() {
                             <span/>
                         </div>
                     </nav>
-                    <div className="header_logo" >COGNITIVE QUIZ</div>
+                    <div className="header_logo">COGNITIVE QUIZ</div>
                     <div>
                         <button className="button">CREATE NEW QUIZ</button>
                     </div>
                 </header>
             </AppBar>
-<div className="main">
-    <QuizList/>
-</div>
-            <QuizMenu active={menuActive} setActive={setMenuActive} header={"Menu"} items={items}/>
+            <div className="box_main">
+                <div className="box_menu">
+                    <QuizMenu active={menuActive} setActive={setMenuActive} header={"Menu"} items={items}/>
+                </div>
+                <div className="main">
+                    <QuizList/>
+                </div>
 
+            </div>
+
+            {/*<QuizMenu active={menuActive} setActive={setMenuActive} header={"Menu"} items={items}/>*/}
+            <QuizFooter/>
 
         </Box>
     );
