8b1dd05a956fbc32fee59e243f32a4f23f6daf3e
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2023-04-14 20:14:12 +0300

    fix eslintrc

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,16 @@
 import axios from './servis';
 
 const quizs = {
-    fetch: () => axios.get('/v1').then(data => data),
-}
+  fetch: () => axios.get('/v1').then((data) => data),
+};
 
-const quizsTests ={
-    fetch: (quizTest) => axios.get(`/${quizTest}`)
-        .then(res => {
-            if (200=== res.status) {
-                return res.data;
-            }
-        }),
-
-
-}
-export   { quizs, quizsTests };
+const quizsTests = {
+  fetch: (quizTest) => axios.get(`/${quizTest}`)
+    .then((res) => {
+      // eslint-disable-next-line promise/always-return
+      if (res.status === 200) {
+        return res.data;
+      }
+    }),
+};
+export { quizs, quizsTests };
--- src/api/servis.js
+++ src/api/servis.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://640f1ef14ed25579dc466e8a.mockapi.io',
-    headers: { "Content-Type": "application/json"}
-})
+  baseURL: 'https://640f1ef14ed25579dc466e8a.mockapi.io',
+  headers: { 'Content-Type': 'application/json' },
+});
